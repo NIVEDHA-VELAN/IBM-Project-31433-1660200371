3c3bf3eaa8c1af99867caeb10703f66d5c116027
keerthika1235 <113775195+keerthika1235@users.noreply.github.com>
2022-09-24 15:09:32 +0530

    Create string-concatenate.py

---
+++ Assignments/Keerthika G/python/string-concatenate.py
@@ -0,0 +1,23 @@
+# String Operations in python 
+
+#Concotenate a String 
+
+str1="hello"
+str2="world"
+
+str3=str1+str2
+print(str3)
+
+
+     
+   #using join method
+   
+str1 = "Hello"  
+str2 = "world" 
+   
+print("".join([str1, str2]))  
+  
+# the string with a separator Space(" ")  
+str3 = " ".join([str1, str2])  
+  
+print(str3)  
